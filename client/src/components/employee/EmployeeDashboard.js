6b869759c137ac303f4e9764e239c8f089c1bd99
Jatin Taneja <taneja.jatin1@gmail.com>
2023-10-23 14:04:21 -0400

    employee dashboard changes

--- client/src/components/employee/EmployeeDashboard.js
+++ client/src/components/employee/EmployeeDashboard.js
@@ -1,56 +1,84 @@
-import { useState } from "react";
-import "../../assets/styles/dashboard.css";
+
+import "../../assets/styles/employe.css";
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import TimerIcon from '@mui/icons-material/Timer';
+
+import Draggable from "react-draggable";
 
 function Tasks() {
-  const [cards] = useState([
-    {
-      title: "Task 1 : Due Today",
-      text1: `Create graphs for dashboard`,
-      text2: `Description: Pie chart showing growth of the project`,
-      text3: `Status: Done`,
-      text4: `Project: Vision 3D`,
-      flag: `true`,
-    },
-    {
-      title: "Task 2 : Due on 15th Dec",
-      text1: `Update API of the rent table and change datatype of columns specific to value.`,
-      text2: `Description: Add column for Image and update datatype of price to Decimal`,
-      text3: `Status: Done`,
-      text4: `Project: Health Care`,
-    },
-    {
-      title: "Task 3 : Due on 20th Dec",
-      text1: `Delete customer who are not eligible for upgrade of mobile device and sim.`,
-      text2: `Description: Delete with specific id which matches to the first time user.`,
-      text3: `Status: Doing`,
-      text4: `Project: Capstone`,
-      flag: `false`,
-    },
-    {
-      title: "Task 4 : Due on 22th Dec",
-      text1: `Fix design for rent listing with respect to AODA complaince style.`,
-      text2: `Description: Desing using mui and Ant design for specific requirements.`,
-      text3: `Status: Doing`,
-      text4: `Project: Robotics`,
-    },
-  ]);
+
+
 
   return (
     <div class="section">
       <section>
-        <div className="container1">
-          <h1 align="center">My dashboard</h1>
-          <div className="cards">
-            {cards.map((card, i) => (
-              <div key={i} className="card">
-                <h2>{card.title}</h2>
-                <br />
-                <p>{card.text1}</p>
-                <p>{card.text2}</p>
-                <p>{card.text4}</p>
-                <p>{card.text3}</p>
+        <div className="task-container">
+          <div className="task-list-container">
+            <p className="title">To do</p>
+
+            <div className="task-list">
+            <Draggable>
+              <div className="task-card">
+                <div className="task-card-header">
+                  <p className="heading-text">Lorem Ipsum is simply</p>
+                  <MoreHorizIcon />
+                </div>
+                <div className="task-card-info">
+                  <p className="info-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                  </p>
+                  <div className="deadline">
+                    <span className="alarm-icon"><TimerIcon fontSize="20" /></span><p className="date-text"><span>Deadline:</span> 11th - 15th Aug 2023</p></div>
+                </div>
+                <div className="task-card-footer">
+                  <p>Assign</p>
+                </div>
               </div>
-            ))}
+            </Draggable>
+            </div>
+          </div>
+          <div className="task-list-container">
+            <p className="title">Doing</p>
+            <div className="task-list">
+            <Draggable>
+              <div className="task-card">
+                <div className="task-card-header">
+                  <p className="heading-text">Lorem Ipsum is simply</p>
+                  <MoreHorizIcon />
+                </div>
+                <div className="task-card-info">
+                  <p className="info-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                  </p>
+                  <div className="deadline">
+                    <span className="alarm-icon"><TimerIcon fontSize="20" /></span><p className="date-text"><span>Deadline:</span> 11th - 15th Aug 2023</p></div>
+                </div>
+                <div className="task-card-footer">
+                  <p>Assign</p>
+                </div>
+              </div>
+            </Draggable>
+            </div>
+          </div>
+          <div className="task-list-container">
+            <p className="title">Done</p>
+            <div className="task-list">
+            <Draggable>
+              <div className="task-card">
+                <div className="task-card-header">
+                  <p className="heading-text">Lorem Ipsum is simply</p>
+                  <MoreHorizIcon />
+                </div>
+                <div className="task-card-info">
+                  <p className="info-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                  </p>
+                  <div className="deadline">
+                    <span className="alarm-icon"><TimerIcon fontSize="20" /></span><p className="date-text"><span>Deadline:</span> 11th - 15th Aug 2023</p></div>
+                </div>
+                <div className="task-card-footer">
+                  <p>Assign</p>
+                </div>
+              </div>
+            </Draggable>
+            </div>
           </div>
         </div>
       </section>

c48fa274782f7088b853ff42ab818ab0fdca1b1a
carly4code <orkwan@hotmail.com.hk>
2023-10-26 18:46:40 -0400

    update task for reivew: popup work now

--- client/src/components/manager/components/TaskForReviewList.jsx
+++ client/src/components/manager/components/TaskForReviewList.jsx
@@ -1,90 +1,106 @@
-import { React, useState, useEffect } from "react";
-import {
-  Button,
-  Typography,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  Card,
-  Avatar,
-  CardHeader,
-} from "@material-ui/core";
-// import viewAllProjects from "../../../graphql/viewAllProjects";
-import PROJECT_SAMPLE_DATA from "../../../data/PROJECT_SAMPLE_DATA";
-import getAllTasks from "../../../graphql/getAllTasks";
+import React from 'react'
+import { useState, useEffect } from 'react';
+import { Button, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Card, Avatar, CardHeader } from '@material-ui/core';
+//import PROJECT_SAMPLE_DATA from "../../../data/PROJECT_SAMPLE_DATA";
+import getAllTasks from '../../../graphql/getAllTasks';
+import TaskDetailPopUp from '../../../components/common/TaskDetailPopUp'
 
 const avatars = [1, 2, 3, 4];
 
 export default function TaskForReviewList() {
-  const url = "http://localhost:4000/graphql";
-  const [task, setTask] = useState([]);
 
-  useEffect(() => {
-    loadData();
-    setTask(PROJECT_SAMPLE_DATA);
-  }, []);
+    const [open, setOpen] = useState(false);
 
-  const loadData = (filters) => {
-    getAllTasks(url).then((result) => {
-      setTask(result.data.getTasks);
-    });
-  }; // end of loadData
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  return (
-    <Card>
-      <Typography variant="subtitle1" component="body1">
-        Tasks waiting for review
-      </Typography>
+    const url = "http://localhost:4000/graphql";
+    const [task, setTask] = useState([]);
+    const [index, setIndex] = useState();
+    const [selectedTask, setSelectedTask] = useState(null);
 
-      {task.length > 0 ? (
-        <TableContainer>
-          <Table>
-            <TableBody>
-              {task.map((task, count) =>
-                task.taskStatus === "In-review" ? (
-                  <TableRow
-                    hover={true}
-                    style={{ fontSize: "4rem" }}
-                    key={task.taskNum}
-                  >
-                    <TableCell component="th" scope="row">
-                      <CardHeader
-                        avatar={
-                          <Avatar
-                            src={`/assets/images/avatar${count}.png`}
-                            key={count}
-                          />
-                        }
-                        title={
-                          task.taskManager
-                            ? `${task.taskManager.fname} ${task.taskManager.lname}`
-                            : `${task.taskClient}`
-                        }
-                      />
-                    </TableCell>
-                    <TableCell>{task.taskName}</TableCell>
-                    <TableCell style={{ textAlign: "end" }}>
-                      <Button
-                        className="state-btn in-review-btn"
-                        disableElevation
-                        variant="contained"
-                      >
-                        Show detail
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                ) : null
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      ) : (
-        <div>No tasks available for review</div>
-      )}
-    </Card>
-  );
+
+    useEffect(() => {
+        loadData();
+    }, []);
+
+    const loadData = async () => {
+        await getAllTasks(url).then((result) => {
+            setTask(result.data.getTasks);
+        });
+    }; // end of loadData
+
+    return (
+        <Card>
+            <Typography variant="subtitle1" component="body1">
+                Tasks waiting for review
+            </Typography>
+
+
+            {task.length > 0 ? (
+                <TableContainer>
+                    <Table>
+                        <TableBody>
+                            {task.map((task, count) =>
+                                <TableRow hover={true} style={{ fontSize: '4rem' }} key={task.taskNum}
+                                    onClick={() => {
+                                        setIndex(count);
+                                        handleClickOpen();
+                                        setSelectedTask(task); // set the clicked task
+                                        console.log(count);
+                                        // return (
+                                        //     <TaskDetailPopUp
+                                        //     task={task}
+                                        //         handleClose={handleClose}
+                                        //         handleClickOpen={handleClickOpen}
+                                        //         open={open}
+                                        //         index={index}
+                                        //     />
+                                        // )
+                                    }}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        <CardHeader
+                                            avatar={<Avatar src={`/assets/images/avatar${count}.png`} key={count} />}
+                                            title={`${task.taskEmployee.fname} ${task.taskEmployee.lname}`}
+                                        />
+                                    </TableCell>
+                                    <TableCell>{task.taskName}</TableCell>
+                                    <TableCell style={{ textAlign: 'end' }}>
+                                        <div
+                                            className={`state-btn ${task.taskStatus === 'Todo' ? 'todo-btn' : task.taskStatus === 'In Progress' ? 'doing-btn' : task.taskStatus === 'Done' ? 'done-btn' :
+                                                ' in-review-btn'
+                                                }`}
+                                            disableElevation
+                                            variant="contained"
+                                        >
+                                            {task.taskStatus}
+
+                                        </div>
+                                    </TableCell>
+                                </TableRow>
+
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            ) : (
+                <div>No tasks available for review</div>
+            )}
+            {open && (
+      <TaskDetailPopUp
+      task={selectedTask}
+      handleClose={handleClose}
+        handleClickOpen={handleClickOpen}
+        open={open}
+        index={index}
+      />
+    )}
+        </Card>
+
+    );
 }
